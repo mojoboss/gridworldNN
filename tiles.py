1bad6d7f4cc95044b644f4dbd8ce6e6f55060342
mojoboss <animusmojo@gmail.com>
2016-10-15 13:11:16 +0530

    movement checking added

--- tiles.py
+++ tiles.py
@@ -28,18 +28,31 @@ def make_move(state, action):
 	wall_pos = find_pos(state[1], 1)
 	pit_pos = find_pos(state[2], 1)
 	goal_pos = find_pos(state[3], 1)
+	state = np.zeros((4, 4, 4))
 	#move_up
 	if action == 0:
 		new_loc = (player_pos[0]-1, player_pos[1])
+		if (np.array(new_loc) <= (3, 3)).all() and new_loc != wall_pos and ((np.array(new_loc) >= (0, 0)).all():
+			state[0][new_loc] = 1
 	#move down
 	elif action == 1:
 		new_loc = (player_pos[0]+1, player_pos[1])
+		if (np.array(new_loc) <= (3, 3)).all() and new_loc != wall_pos and ((np.array(new_loc) >= (0, 0)).all():
+                        state[0][new_loc] = 1
 	#move left
 	elif action == 2:
 		new_loc = (player_pos[0], player_pos[1]-1)
+		if (np.array(new_loc) <= (3, 3)).all() and new_loc != wall_pos and ((np.array(new_loc) >= (0, 0)).all():
+                        state[0][new_loc] = 1
 	#move right
 	elif action == 3:
 		new_loc = (player_pos[0], player_pos[1]+1)
+		if (np.array(new_loc) <= (3, 3)).all() and new_loc != wall_pos and ((np.array(new_loc) >= (0, 0)).all():
+                        state[0][new_loc] = 1
+	state[1][wall_pos] = 1
+	state[2][pit_pos] = 1
+	state[3][goal_pos] = 1
+	return state
 	
 if __name__ == "__main__":
 	state = init_grid()
